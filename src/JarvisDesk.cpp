d6ff59625e28ea171854da956aed660dd6192429
Phil Hord <phord@purestorage.com>
2020-06-13 15:07:36 -0700

    cmdPacket: a better message interpreter
    Now that I know the packet fields better (command, length,
    etc.) I can do a better job of interpreting packets as they
    are received, byte-by-byte. Add a state-machine to interpret
    packets.

--- src/JarvisDesk.cpp
+++ src/JarvisDesk.cpp
@@ -465,11 +465,133 @@ private:
     }
   }
 
+  struct cmdPacket {
+    /** Note: Most of these commands are sent only from the desk controller or from
+              the handset.  They are collected here in one enum for simplicity.
+    **/
+    enum command_byte {
+      // FAKE
+      NONE        = 0x00,  // Unused/never seen; used as default for "Uninitialized"
+
+      // CONTROLLER
+      HEIGHT      = 0x01,  // Height report; P0=4 (mm?)
+      LIMIT_RESP  = 0x20,  // Max-height set/cleared; response to [21];
+      REP_MAX     = 0x21,  // Report max height; Response to SET_MAX
+      REP_MIN     = 0x22,  // Report min height; Response to SET_MIN
+      LIMIT_STOP  = 0x23,  // Min/Max reached
+      RESET       = 0x40,  // Indicates desk in RESET mode; Displays "RESET"
+      REP_PRESET  = 0x92,  // Moving to Preset location
+
+      // HANDSET
+      UNITS       = 0x0E,  // Set units to cm/inches
+      MEM_MODE    = 0x19,  // Set memory mode
+      COLL_SENS   = 0x1D,  // Set anti-collision sensitivity  (Sent 1x; no repeats)
+      SET_MAX     = 0x21,  // Set max height; Sets max-height to current height
+      SET_MIN     = 0x22,  // Set min height; Sets min-height to current height
+      LIMIT_CLR   = 0x23,  // Clear min/max height
+      PROGMEM_1   = 0x03,  // Set memory position 1 to current height
+      PROGMEM_2   = 0x04,  // Set memory position 2 to current height
+      PROGMEM_3   = 0x25,  // Set memory position 3 to current height
+      PROGMEM_4   = 0x26,  // Set memory position 4 to current height
+      WAKE        = 0x29,  // Poll message (??) sent when desk doesn't respond to BREAK messages
+      CALIBRATE   = 0x91,  // Height calibration (Repeats 2x)
+    };
+
+    cmdPacket(unsigned char addr_) : addr(addr_) {}
+
+    command_byte  cmd = NONE;
+    unsigned char addr;
+    unsigned char checksum = 99;
+    unsigned char argc = 0;
+    unsigned char argv[5];
+    enum state_t {
+      SYNC,    // waiting for addr
+      SYNC2,   // waiting for addr2
+      CMD,     // waiting for cmd
+      LENGTH,  // waiting for argc
+      // ARGS4,3,2,1   // collecting args
+      ARGS = LENGTH + sizeof(argv),   // collecting args
+      CHKSUM,  // waiting for checksum
+      ENDMSG,  // waiting for EOM
+    } state = SYNC;
+
+    bool error(unsigned char ch) {
+      state = static_cast<state_t>(SYNC + (ch == addr));
+      cmd = NONE;
+      argc = 0;
+      memset(argv, 0U, sizeof(argv));
+      return false;
+    }
+
+    bool put(unsigned char ch) {
+      bool complete = false;
+
+      switch (state) {
+      case SYNC:
+      case SYNC2:
+        if (ch != addr) return error(ch);
+        break;
+
+      case CMD:
+        cmd = static_cast<cmdPacket::command_byte>(checksum = ch);
+        break;
+
+      case LENGTH:
+        if (ch > sizeof(argv)) return error(ch);
+        checksum += (argc = ch);
+        state = static_cast<state_t>(CHKSUM - ch - 1);
+        break;
+
+      default:    // ARGS
+        if (state <= LENGTH || state > ARGS) return error(ch); // assert(ARGS);
+        checksum += (argv[argc - (CHKSUM-state)] = ch);
+        break;
+
+      case CHKSUM:
+        if (ch != checksum) return error(ch);
+        complete = true;
+        break;
+
+      case ENDMSG:
+        return error(ch);  // We do the same here whether it's an error or not
+      }
+      // Common increment for every state
+      state = static_cast<state_t>(state + 1);
+      if (state < SYNC || state > ENDMSG) return error(ch); // assert(state);
+      return complete;
+    }
+
+    void dump() {
+      if (serverClient && serverClient.connected()) {
+        serverClient.print("Packet: addr=");
+        serverClient.print(addr, HEX);
+        serverClient.print(" state=");
+        serverClient.print(state);
+        serverClient.print(" argc=");
+        serverClient.print(argc, HEX);
+        serverClient.print(" argv=[ ");
+        for (unsigned i = 0 ; i < argc ; i++) {
+          serverClient.print(argv[i], HEX);
+          serverClient.print(" ");
+        }
+        serverClient.print("] checksum=");
+        serverClient.print(checksum, HEX);
+        serverClient.println();
+      }
+    }
+  };
+
+  cmdPacket deskPacket = {CONTROLLER};
+  cmdPacket hsPacket = {HANDSET};
+
   // Decode the serial stream from the desk controller
   void decode_serial() {
     while (deskSerial.available()) {
       auto ch = deskSerial.read();
       auto p = desk.put(ch);
+      if (deskPacket.put(ch)) {
+        deskPacket.dump();
+      }
       if (!error(p))
         decode_desk(p);
       else if (p.err == ring_buffer::payload::ERR::CHKSUM)
@@ -487,6 +609,9 @@ private:
     while (hsSerial.available()) {
       auto ch = hsSerial.read();
       auto p = hs.put(ch);
+      if (hsPacket.put(ch)) {
+        hsPacket.dump();
+      }
       if (!error(p))
         decode_handset(p);
       else if (p.err == ring_buffer::payload::ERR::CHKSUM)
